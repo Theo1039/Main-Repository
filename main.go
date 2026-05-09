24bf91bbffd8a3721ddd7bb143dd0b66f5c72f84
Theophilus Kpurugbaragit config --global user.email theophiluskpurugbara@gmail.com <homepc@PC.localdomain>
2026-05-09 17:55:50 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func add(a int, b int) int {
+	return a + b
+}
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome To Theo App")
+}
+
+func addition(w http.ResponseWriter, r *http.Request) {
+	ad := add(3, 5)
+	fmt.Fprintln(w, ad)
+}
+func main() {
+	http.HandleFunc("/home", homePage)
+	http.HandleFunc("/add", addition)
+	fmt.Println("server run at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
