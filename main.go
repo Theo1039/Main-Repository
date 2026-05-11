a88891402cd9f1ac60266f5058f2708e044d01a6
Theophilus Kpurugbaragit config --global user.email theophiluskpurugbara@gmail.com <homepc@PC.localdomain>
2026-05-11 16:44:17 +0100

    first commit

--- main.go
+++ main.go
@@ -17,11 +17,9 @@ func addition(w http.ResponseWriter, r *http.Request) {
 	ad := add(3, 5)
 	fmt.Fprintln(w, ad)
 }
-
 func htmlPractice(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
-
 func main() {
 	http.HandleFunc("/home", homePage)
 	http.HandleFunc("/add", addition)
